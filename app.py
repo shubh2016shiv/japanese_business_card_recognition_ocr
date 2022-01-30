3d043734eca2cd50f25d140d17911e5b6a08985a
Shubham Singh <shubh2014shiv@gmail.com>
2022-01-30 15:36:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -238,3 +238,16 @@ elif nav_option == "Perform Japanese OCR":
             st.sidebar.write(psutil.virtual_memory())
             with open("./output.txt",encoding='utf-8',mode='r') as f:
                 st.write(f.read())
+                
+            import sys
+            def sizeof_fmt(num, suffix='B'):
+                ''' by Fred Cirera,  https://stackoverflow.com/a/1094933/1870254, modified'''
+                for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+                    if abs(num) < 1024.0:
+                        return "%3.1f %s%s" % (num, unit, suffix)
+                    num /= 1024.0
+                return "%.1f %s%s" % (num, 'Yi', suffix)
+
+            for name, size in sorted(((name, sys.getsizeof(value)) for name, value in locals().items()),
+                                     key= lambda x: -x[1])[:10]:
+                st.write("{:>30}: {:>8}".format(name, sizeof_fmt(size)))
